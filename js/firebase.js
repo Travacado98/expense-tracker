7b1d11a9a12fabf11f37ed8997fd15bc66855202
Travacado98 <travissmith.ts98@gmail.com>
2022-04-05 11:55:11 -0700

    Initial commit

---
+++ js/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+} from "https://www.gstatic.com/firebasejs/9.6.10/firebase-firestore.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApPnFXSSj0JcBNMFKROpewPm0cQCDxlh0",
+  authDomain: "expense-tracker-95ecd.firebaseapp.com",
+  projectId: "expense-tracker-95ecd",
+  storageBucket: "expense-tracker-95ecd.appspot.com",
+  messagingSenderId: "200945708018",
+  appId: "1:200945708018:web:066be93f6480ca754ed96b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export { db, collection, getDocs, addDoc, deleteDoc, doc, onSnapshot, query };
---
+++ js/script.js
@@ -0,0 +1,147 @@
+/*
+  Expense Tracker
+  Create a web application for tracking expenses
+  
+  Requirements:
+  + Allow users to add an expense item to a list with:
+    - Description (required)
+    - Amount (required)
+    - Date (optional)
+  + Allow users to delete expense items
+  + Allow users to edit existing expense items in the list
+
+  Notes:
+  + The Amount should be saved as a number, but displayed with a dollar sign in the list
+  + Deletion should be confirmed by the user with a browser confirmation popup
+  + Save the application state to the browser so that it persists on refresh
+  + The site should be mobile responsive and have a clean & friendly UI/UX
+  + The list should be sorted by the Date with the newest entries at the top, oldest at the bottom
+
+  BONUS CHALLENGES:
+  + Allow the user to change the sort based on any of the fields ascending or descending
+  + Ensure that the user cannot type more than 2 decimal places in the Amount input field (ensure input is a valid USD currency format)
+  + Display "Yesterday" and "Today" for the dates in the list when they are yesterday or today respectively
+*/
+
+import {
+  db,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+} from "./firebase.js";
+
+let expenseList = [];
+let total = 0;
+
+const getExpenses = () => {
+  const q = query(collection(db, "expenses"));
+
+  onSnapshot(q, (querySnapshot) => {
+    expenseList = [];
+
+    querySnapshot.forEach((doc) => {
+      const date = doc.data().date;
+
+      const expenseObj = {
+        ...doc.data(),
+        _id: doc.id,
+        date: date ? date.toDate().toDateString() : null,
+      };
+
+      expenseList.push(expenseObj);
+    });
+
+    renderExpenses();
+    calculateTotal();
+  });
+};
+
+getExpenses();
+
+// Create our number formatter.
+const formatter = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD",
+});
+
+// Calculate total
+const calculateTotal = () => {
+  let newTotal = 0;
+
+  expenseList.forEach((expenseObj) => {
+    newTotal += expenseObj.amount;
+  });
+  total = newTotal;
+  document.getElementById("total").textContent = formatter.format(total);
+};
+
+// Adding Entries
+const addExpense = async (e) => {
+  e.preventDefault();
+
+  const date = document.getElementById("Date").value;
+
+  const expenseObj = {
+    name: document.getElementById("Name").value,
+    amount: parseFloat(document.getElementById("Amount").value),
+    date: date ? new Date(`${date} 00:00`).toDateString() : null,
+    id: Date.now(),
+  };
+
+  addDoc(collection(db, "expenses"), {
+    ...expenseObj,
+    date: date ? new Date(expenseObj.date) : null,
+  });
+};
+
+const renderExpenses = () => {
+  document.getElementById("expenseList").innerHTML = "";
+  expenseList.forEach(renderExpense);
+};
+
+const renderExpense = (expenseObj) => {
+  const expenseElm = document.createElement("div");
+
+  expenseElm.className = "expense-item";
+  expenseElm.id = expenseObj.id;
+
+  const nameSpan = document.createElement("span");
+  const amountSpan = document.createElement("span");
+  const dateSpan = document.createElement("span");
+  const deleteButton = document.createElement("button");
+  const editButton = document.createElement("button");
+
+  nameSpan.textContent = expenseObj.name;
+  amountSpan.textContent = formatter.format(expenseObj.amount);
+  dateSpan.textContent = expenseObj.date;
+  editButton.textContent = "Edit";
+  deleteButton.textContent = "X";
+
+  deleteButton.onclick = () => deleteExpense(expenseObj.id);
+  editButton.onclick = () => editExpense(expenseObj.id);
+
+  expenseElm.appendChild(nameSpan);
+  expenseElm.appendChild(amountSpan);
+  expenseElm.appendChild(dateSpan);
+  expenseElm.appendChild(editButton);
+  expenseElm.appendChild(deleteButton);
+
+  document.getElementById("expenseList").appendChild(expenseElm);
+};
+
+// Delete Entries
+
+const deleteExpense = (id) => {
+  const index = expenseList.findIndex((expenseObj) => {
+    return expenseObj.id === id;
+  });
+  deleteDoc(doc(db, "expenses", expenseList[index]._id));
+};
+
+const editExpense = (id) => {};
+
+window.addExpense = addExpense;
